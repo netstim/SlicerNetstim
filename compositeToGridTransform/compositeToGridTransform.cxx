netstim/SlicerNetstim

commit 908a5c4d4428b3ba4c5bd99bce7ebe52033b2226

ENH: use logic as in Volumes to compute displacement field.

compositeToGridTransform/compositeToGridTransform.cxx
@@ -1,11 +1,17 @@
 #include "compositeToGridTransformCLP.h"
 
-// ITK includes
-#include <itkImage.h>
+// MRML includes
+#include <vtkMRMLTransformNode.h>
+#include <vtkMRMLScalarVolumeNode.h>
+#include <vtkMRMLVolumeArchetypeStorageNode.h>
+#include <vtkMRMLTransformStorageNode.h>
 
-#include "SimpleITK.h"
-
-namespace sitk = itk::simple;
+// VTK includes
+#include <vtkGeneralTransform.h>
+#include <vtkImageData.h>
+#include <vtkMatrix4x4.h>
+#include <vtkOrientedGridTransform.h>
+#include <vtkOrientedGridTransform.h>
 
 // Use an anonymous namespace to keep class types and function names
 // from colliding when module is used as shared object module.  Every
@@ -14,34 +20,46 @@ namespace sitk = itk::simple;
 //
 namespace
 {
-void DoIt(sitk::Image *vectorField, sitk::Transform *compositeTransform)
+void GetTransformedPointSamplesAsVectorImage(vtkImageData* vectorImage, vtkMRMLTransformNode* inputTransformNode, vtkMatrix4x4* ijkToRAS)
 {
+  vtkNew<vtkGeneralTransform> inputTransform;
+  inputTransformNode->GetTransformFromWorld(inputTransform.GetPointer());
 
-  unsigned int i, imageLinearIndex;
-  std::vector<double> physicalPoint, physicalPointTransformed;
-  std::vector<unsigned int> size = vectorField->GetSize();
-  float * buffer = vectorField->GetBufferAsFloat();
-  std::vector<itk::int64_t> ijk {0,0,0};
+  // The orientation of the volume cannot be set in the image
+  // therefore the volume will not appear in the correct position
+  // if the direction matrix is not identity.
+  vectorImage->AllocateScalars(VTK_FLOAT, 3);
 
-  float numberOfVoxels = size[0] * size[1] * size[2];
-  unsigned int voxelCount = 0;
+  double point_RAS[4] = { 0, 0, 0, 1 };
+  double transformedPoint_RAS[4] = { 0, 0, 0, 1 };
+  double point_IJK[4] = { 0, 0, 0, 1 };
+  float* voxelPtr = static_cast<float*>(vectorImage->GetScalarPointer());
+  int* extent = vectorImage->GetExtent();
+  int* dim = vectorImage->GetDimensions();
+  float numberOfVoxels = dim[0] * dim[1] * dim[2];
+  uint voxelCount = 0;
+  for (point_IJK[2] = extent[4]; point_IJK[2] <= extent[5]; point_IJK[2]++)
+  {
+    for (point_IJK[1] = extent[2]; point_IJK[1] <= extent[3]; point_IJK[1]++)
+    {
+      for (point_IJK[0] = extent[0]; point_IJK[0] <= extent[1]; point_IJK[0]++)
+      {
+        ijkToRAS->MultiplyPoint(point_IJK, point_RAS);
 
-  for(ijk[0]=0; ijk[0]<size[0]; ijk[0]=ijk[0]+1){
-  for(ijk[1]=0; ijk[1]<size[1]; ijk[1]=ijk[1]+1){
-  for(ijk[2]=0; ijk[2]<size[2]; ijk[2]=ijk[2]+1){
+        inputTransform->TransformPoint(point_RAS, transformedPoint_RAS);
 
-    physicalPoint = vectorField->TransformIndexToPhysicalPoint(ijk);
-    physicalPointTransformed = compositeTransform->TransformPoint(physicalPoint);
+        // store the pointDislocationVector_RAS components in the image
+        *(voxelPtr++) = static_cast<float>(transformedPoint_RAS[0] - point_RAS[0]);
+        *(voxelPtr++) = static_cast<float>(transformedPoint_RAS[1] - point_RAS[1]);
+        *(voxelPtr++) = static_cast<float>(transformedPoint_RAS[2] - point_RAS[2]);
 
-    imageLinearIndex = ijk[0] + (size[0] * (ijk[1] + size[1] * ijk[2]));
-    for (i=0; i<3; i++){
-      buffer[3*imageLinearIndex+i] = physicalPointTransformed[i] - physicalPoint[i];
+        voxelCount++;
+        if (voxelCount % 10000 == 0)
+        {
+          std::cout << "<filter-progress>" << (voxelCount / numberOfVoxels) << "</filter-progress>" << std::endl << std::flush;
+        }
+      }
     }
-
-    voxelCount++;
-  }
-  }
-    std::cout << "<filter-progress>" << (voxelCount / numberOfVoxels) << "</filter-progress>" << std::endl << std::flush;
   }
 
 }
@@ -61,25 +79,107 @@ int main( int argc, char * argv[] )
     return EXIT_FAILURE;
   }
 
-  // Read input
-  sitk::Transform input = sitk::ReadTransform(inputCompositeTransform);
+  vtkNew<vtkMRMLScalarVolumeNode> referenceVolumeNode;
+  if (!referenceVolume.empty())
+  {
+    vtkNew<vtkMRMLVolumeArchetypeStorageNode> storageNode;
+    storageNode->SetFileName(referenceVolume.c_str());
+    if (!storageNode->ReadData(referenceVolumeNode))
+    {
+      std::cerr << "Failed to read reference volume from file " << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
 
-  // Create vector field
-  sitk::ImageFileReader reader;
-  reader.SetFileName(referenceVolume);
-  sitk::Image &&referenceImage = reader.Execute();
+  vtkNew<vtkMRMLTransformNode> compositeTransformNode;
+  if (!inputCompositeTransform.empty())
+  {
+    vtkNew<vtkMRMLTransformStorageNode> storageNode;
+    storageNode->SetFileName(inputCompositeTransform.c_str());
+    if (!storageNode->ReadData(compositeTransformNode))
+    {
+      std::cerr << "Failed to read composite transform from file " << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
 
-  sitk::Image outputImage(referenceImage.GetSize(), sitk::sitkVectorFloat32);
-  outputImage.SetOrigin(referenceImage.GetOrigin());
-  outputImage.SetSpacing(referenceImage.GetSpacing());
-  outputImage.SetDirection(referenceImage.GetDirection());
+  // Create a grid transform
+  vtkSmartPointer<vtkMRMLTransformNode> outputGridTransformNode;
+  outputGridTransformNode = vtkSmartPointer<vtkMRMLTransformNode>::New();
 
-  // Apply
+  vtkOrientedGridTransform* outputGridTransform = vtkOrientedGridTransform::SafeDownCast(
+    outputGridTransformNode->GetTransformToParentAs("vtkOrientedGridTransform",
+    false /* don't report conversion error */,
+    true /* we would like to modify the transform */));
+  if (outputGridTransform == nullptr)
+  {
+    // we cannot reuse the existing transform, create a new one
+    vtkNew<vtkOrientedGridTransform> newOutputGridTransform;
+    outputGridTransform = newOutputGridTransform.GetPointer();
+    outputGridTransformNode->SetAndObserveTransformFromParent(outputGridTransform);
+  }
+  // Create/get displacement field image
+  vtkImageData* outputVolume = outputGridTransform->GetDisplacementGrid();
+  if (outputVolume == nullptr)
+  {
+    vtkNew<vtkImageData> newOutputVolume;
+    outputVolume = newOutputVolume.GetPointer();
+    outputGridTransform->SetDisplacementGridData(outputVolume);
+  }
+  // Update geometry based on reference image
+  vtkNew<vtkMatrix4x4> ijkToRas; // RAS refers to world
+  if (referenceVolumeNode != nullptr)
+  {
+    referenceVolumeNode->GetIJKToRASMatrix(ijkToRas.GetPointer());
+    vtkNew<vtkMatrix4x4> rasToWorld;
+    if (vtkMRMLTransformNode::GetMatrixTransformBetweenNodes(referenceVolumeNode->GetParentTransformNode(), nullptr /* world */, rasToWorld.GetPointer()))
+    {
+      vtkMatrix4x4::Multiply4x4(rasToWorld.GetPointer(), ijkToRas.GetPointer(), ijkToRas.GetPointer());
+    }
+    else
+    {
+      std::cerr << "vtkSlicerTransformLogic::ConvertToGridTransform: non-linearly transformed reference volume" \
+       " is not supported. Harden or remove the transform from of the reference volume." << std::endl;
+      return EXIT_FAILURE;
+    }
+    double origin[3] = { 0, 0, 0 };
+    double spacing[3] = { 1, 1, 1 };
+    vtkNew<vtkMatrix4x4> ijkToRasDirection; // normalized direction matrix
+    for (int c = 0; c < 3; c++)
+    {
+      origin[c] = ijkToRas->GetElement(c, 3);
+      spacing[c] = sqrt(ijkToRas->Element[0][c] * ijkToRas->Element[0][c]
+        + ijkToRas->Element[1][c] * ijkToRas->Element[1][c]
+        + ijkToRas->Element[2][c] * ijkToRas->Element[2][c]);
+      if (spacing[c] == 0)
+      {
+        // Prevent division by zero in case there is a projection matrix is in the transform chain
+        spacing[c] = 1.0;
+      }
+      for (int r = 0; r < 3; r++)
+      {
+        ijkToRasDirection->SetElement(r, c, ijkToRas->GetElement(r, c) / spacing[c]);
+      }
+    }
+    outputVolume->SetExtent(referenceVolumeNode->GetImageData()->GetExtent());
+    outputVolume->SetOrigin(origin);
+    outputVolume->SetSpacing(spacing);
+    // vtkImageData cannot store directions, therefore that has to be set in the grid transform
+    outputGridTransform->SetGridDirectionMatrix(ijkToRasDirection.GetPointer());
+  }
+
+
   std::cout << "<filter-comment>" << "Computing" << "</filter-comment>" << std::endl << std::flush;
-  DoIt(&outputImage, &input);
+  GetTransformedPointSamplesAsVectorImage(outputVolume, compositeTransformNode, ijkToRas.GetPointer());
 
-  std::cout << "<filter-comment>" << "Saving" << "</filter-comment>" << std::endl << std::flush;
-  sitk::WriteImage(outputImage, saveToNode ? outputDisplacementField : outputFileName);
+  std::cout << "<filter-comment>" << "Writing" << "</filter-comment>" << std::endl << std::flush;
+  vtkNew<vtkMRMLTransformStorageNode> storageNode;
+  storageNode->SetFileName(saveToNode ? outputDisplacementField.c_str() : outputFileName.c_str());
+  if (!storageNode->WriteData(outputGridTransformNode))
+  {
+    std::cerr << "Failed to write output transform" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }

compositeToGridTransform/CMakeLists.txt
@@ -10,28 +10,9 @@ set(MODULE_NAME compositeToGridTransform)
 find_package(SlicerExecutionModel REQUIRED)
 include(${SlicerExecutionModel_USE_FILE})
 
-#
-# ITK
-#
-set(${PROJECT_NAME}_ITK_COMPONENTS
-  ITKIOImageBase
-  ITKCommon
-  ITKIOTransformBase
-  ITKTransform
-  )
-find_package(ITK 4.6 COMPONENTS ${${PROJECT_NAME}_ITK_COMPONENTS} REQUIRED)
-find_package(SimpleITK)
-if(ITK_VERSION VERSION_GREATER_EQUAL "5.3")
-  foreach(factory_uc IN ITEMS "IMAGEIO" "MESHIO" "TRANSFORMIO")
-    set(ITK_NO_${factory_uc}_FACTORY_REGISTER_MANAGER 1)
-  endforeach()
-else()
-  set(ITK_NO_IO_FACTORY_REGISTER_MANAGER 1) # See Libs/ITKFactoryRegistration/CMakeLists.txt
-endif()
-include(${ITK_USE_FILE})
-
 #-----------------------------------------------------------------------------
 set(MODULE_INCLUDE_DIRECTORIES
+  ${MRMLCore_INCLUDE_DIRS}
   )
 
 set(MODULE_SRCS
@@ -39,7 +20,8 @@ set(MODULE_SRCS
 
 set(MODULE_TARGET_LIBRARIES
   ${ITK_LIBRARIES}
-  ${SimpleITK_LIBRARIES}
+  ${VTK_LIBRARIES}
+  MRMLCore
   )
 
 #-----------------------------------------------------------------------------

commit 38a0e13dc33007f023242872afaf2d05401857bd

COMP: Fix undeclared uint
This addresses an issue observed in https://slicer.cdash.org/viewBuildError.php?buildid=2831893. This issue was introduced in https://github.com/netstim/SlicerNetstim/commit/908a5c4d4428b3ba4c5bd99bce7ebe52033b2226.

compositeToGridTransform/compositeToGridTransform.cxx
@@ -37,7 +37,7 @@ void GetTransformedPointSamplesAsVectorImage(vtkImageData* vectorImage, vtkMRMLT
   int* extent = vectorImage->GetExtent();
   int* dim = vectorImage->GetDimensions();
   float numberOfVoxels = dim[0] * dim[1] * dim[2];
-  uint voxelCount = 0;
+  unsigned int voxelCount = 0;
   for (point_IJK[2] = extent[4]; point_IJK[2] <= extent[5]; point_IJK[2]++)
   {
     for (point_IJK[1] = extent[2]; point_IJK[1] <= extent[3]; point_IJK[1]++)